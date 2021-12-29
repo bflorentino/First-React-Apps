6a399c0da194f31e61c8b59b0c02379bb732c4ef
Bryan <bryanmontero81@gmail.com>
2021-12-29 00:48:54 -0400

    Currencies GET Requests work well

--- Proyecto Convertidor de Monedas/src/Services/CurrencyService.js
+++ Proyecto Convertidor de Monedas/src/Services/CurrencyService.js
@@ -2,6 +2,29 @@ import APIKEY from "./ApiKey"
 
 const URL = "https://free.currconv.com/api"
 
-const getCurrenciesList = async() => {
-    let response = await fetch(`${URL}`)
+export const getCurrenciesList = async() => {
+
+    // Gets the list of currencies ant turns the results into an array of objects
+    const response = await fetch(`${URL}/v7/currencies?apiKey=${APIKEY}`);
+    const { results }  = await response.json();
+    const currenciesToArray = Object.values(results);
+
+    return currenciesToArray;
+}
+
+export const convertCurrencies = async(amount, fromCurrency, toCurrency) => {
+
+    // Get toCurrency Price
+    const response = await fetch(`${URL}/v7/convert?q=${fromCurrency}_${toCurrency}&compact=ultra&apiKey=${APIKEY}`);
+    const result = await response.json();
+
+    // Get price from JSON object 
+    const price = result[`${fromCurrency}_${toCurrency}`];
+
+    return (
+            {
+                price,
+                total: price * amount
+            }
+        )
 }
